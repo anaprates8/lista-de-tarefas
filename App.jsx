80a59276f9c51bcc884113989b2e95f324dd0c1d
Ana Paula Prates <115781186+anaprates8@users.noreply.github.com>
2023-03-31 02:26:34 -0300

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -4,11 +4,18 @@ import CardAdicionar from './Components/CardAdicionar'
 
 
 function App() {
-  const [arrayDeObjetos, setArrayDeObjetos] = useState([]);
+  const [textoTarefa, setTextoTarefa] = useState([]);
+  
+
+  let novaTarefa = {
+    id: 0,
+    texto: textoTarefa,
+    finalizado: false,
+  }
 
   return (
     <div>
-      <CardAdicionar />
+      <CardAdicionar adicionarTarefa={novaTarefa}></CardAdicionar>
     </div>
   );
 }
